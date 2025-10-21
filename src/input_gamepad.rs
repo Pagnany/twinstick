b4af52bc02ab7abe6839fad2d002c24594d61411
Eric Schmale <eric.schmale@gmx.de>
2025-10-21 20:14:26 +0200

    rotate on left stick

--- src/input_gamepad.rs
+++ src/input_gamepad.rs
@@ -9,24 +9,17 @@ pub fn gamepad_system(
     let mut player = player_query.single_mut().unwrap();
 
     for gamepad in &gamepads {
-        // The Controller I own works differently on Windows and Linux
-        // Controller on Windows
-        // let right_trigger = gamepad.get(GamepadButton::RightTrigger2).unwrap();
-        // Gamesir controller on Linux
-        let right_trigger = gamepad.get(GamepadAxis::RightZ).unwrap();
-        if !player.right_trigger_down && right_trigger > 0.2 {
-            player.right_trigger_down = true;
-            player.velocity.x += player.dash_power * player.angle.cos();
-            player.velocity.y += player.dash_power * player.angle.sin();
-        } else if player.right_trigger_down && right_trigger < 0.2 {
-            player.right_trigger_down = false;
-        }
-
         let left_stick_x = gamepad.get(GamepadAxis::LeftStickX).unwrap();
         let left_stick_y = gamepad.get(GamepadAxis::LeftStickY).unwrap();
         if left_stick_x.abs() > 0.1 || left_stick_y.abs() > 0.1 {
             player.velocity.x += left_stick_x * player.move_acceleration * time.delta_secs();
             player.velocity.y += left_stick_y * player.move_acceleration * time.delta_secs();
+        }
+
+        let right_stick_x = gamepad.get(GamepadAxis::RightStickX).unwrap();
+        let right_stick_y = gamepad.get(GamepadAxis::RightStickY).unwrap();
+        if right_stick_x.abs() > 0.1 || right_stick_y.abs() > 0.1 {
+            player.angle = right_stick_y.atan2(right_stick_x);
         }
     }
 }
--- src/player.rs
+++ src/player.rs
@@ -7,11 +7,7 @@ pub struct Player {
     /// Angle in radians
     pub angle: f32,
     pub move_acceleration: f32,
-    pub dash_power: f32,
     pub friction: f32,
-    pub breaking_power: f32,
-    pub right_trigger_down: bool,
-    pub init_left_trigger_down: bool,
 }
 
 impl Default for Player {
@@ -21,11 +17,7 @@ impl Default for Player {
             velocity: Vec2::ZERO,
             angle: 0.0,
             move_acceleration: 15000.0,
-            dash_power: 5000.0,
             friction: 15.0,
-            breaking_power: 10000.0,
-            right_trigger_down: false,
-            init_left_trigger_down: false,
         }
     }
 }
@@ -48,14 +40,9 @@ pub fn player_movement_system(time: Res<Time>, mut query: Query<(&mut Player, &m
         transform.translation.y = -crate::WINDOW_HEIGHT / 2.0;
     }
 
-    // Rotate the player based on velocity
-    if player.velocity.length() > 0.0 {
-        player.angle = player.velocity.y.atan2(player.velocity.x);
-
-        // texture is rotated
-        // so PI / 2.0 is subtracted
-        transform.rotation = Quat::from_rotation_z(player.angle - crate::PI / 2.0);
-    }
+    // texture is rotated
+    // so PI / 2.0 is subtracted
+    transform.rotation = Quat::from_rotation_z(player.angle - crate::PI / 2.0);
 
     player.velocity.x -= player.velocity.x * player.friction * time.delta_secs();
     player.velocity.y -= player.velocity.y * player.friction * time.delta_secs();
